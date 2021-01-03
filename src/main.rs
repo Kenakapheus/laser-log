552ebaadb83e8dba825e760a62ba768b5d665028
Kenakapheus <lukas@kenakapheus.org>
2021-01-03 18:27:50 +0100

    Basic UI

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@
 
 use std::fmt::Debug;
 use serde::{Serialize, Deserialize};
-use rocket::response::{NamedFile};
-use rocket::response::status::NotFound;
 use rocket_contrib::templates::Template;
-use std::path::{Path, PathBuf};
 use std::collections::HashMap;
 use std::vec::Vec;
 use std::string::String;
 use std::fs;
-use chrono::{DateTime, Local};
+use chrono::{Local};
 
 
 #[macro_use] extern crate rocket;
@@ -19,13 +16,32 @@ use chrono::{DateTime, Local};
 struct IndexTemplate {
     time: String,
     user: String,
+    materials: HashMap<String, Material>,
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Material {
+    name: String,
+    fullname: Option<String>,
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MaterialDatabase {
+    materials: HashMap<String, Material>,
 }
 
 #[get("/")]
 fn index() -> Template {
+    let material_db: MaterialDatabase = match fs::read_to_string("materials.toml") {
+        Err(_) => {MaterialDatabase {materials: HashMap::new()}}
+        Ok(materials) => {toml::from_str(materials.as_str()).expect("Could not parse meta.toml")}
+    };
     let context = IndexTemplate {
         time: Local::now().format("%d.%m.%Y %H:%M").to_string(),
         user: "Annonym".to_string(),
+        materials: material_db.materials,
     };
     Template::render("index", context)
 }
